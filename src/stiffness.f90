5c4fafc425c2ae7a06528d6b392adfb7320d8cae
Sicheng <hschsc@umich.edu>
2017-02-13 17:50:11 -0500

    Add 2D stress der

--- src/stiffness.f90
+++ src/stiffness.f90
@@ -362,3 +362,116 @@ subroutine getresder(num_nodes, num_elems, num_aug, nnz, &
 
 end subroutine getresder
 
+
+
+
+subroutine getresder2D(num_nodes, num_elems, num_aug, nnz, &
+     E, nodes, elems, disp_aug, data, rows, cols)
+
+  implicit none
+
+  !f2py intent(in) num_nodes, num_elems, num_aug, nnz, E, nodes, elems, disp_aug
+  !f2py intent(out) data, rows, cols
+  !f2py depend(num_nodes) nodes
+  !f2py depend(num_elems) elems
+  !f2py depend(num_aug) disp_aug
+  !f2py depend(nnz) data, rows, cols
+
+  ! Input
+  integer, intent(in) :: num_nodes, num_elems, num_aug, nnz
+  double precision, intent(in) :: E, nodes(num_nodes, 2)
+  integer, intent(in) :: elems(num_elems, 2)
+  double precision, intent(in) :: disp_aug(num_aug)
+
+  ! Output
+  double precision, intent(out) :: data(nnz)
+  integer, intent(out) :: rows(nnz), cols(nnz)
+
+  ! Working
+  double precision :: Telem(2, 4), Kelem(2, 2)
+  double precision :: xyz1(2), xyz2(2), L, cos_xyz(2)
+  double precision :: data4x4(4, 4)
+  integer :: rows4x4(4, 4), cols4x4(4, 4)
+  integer :: ones11(4, 4), ones12(4, 4), ones21(4, 4), ones22(4, 4)
+  integer :: k1, k2, ielem, index, ind1, ind2, ind_aug
+
+  Kelem(1, 1) =  1.
+  Kelem(1, 2) = -1.
+  Kelem(2, 1) = -1.
+  Kelem(2, 2) =  1.
+
+  do k2 = 1, 4
+     do k1 = 1, 4
+        rows4x4(k1, k2) = mod(k1-1, 2)
+        cols4x4(k1, k2) = mod(k2-1, 2)
+     end do
+  end do
+
+  ones11(:, :) = 0
+  ones12(:, :) = 0
+  ones21(:, :) = 0
+  ones22(:, :) = 0
+
+  ones11(1:2, 1:2) = 1
+  ones12(1:2, 3:4) = 1
+  ones21(3:4, 1:2) = 1
+  ones22(3:4, 3:4) = 1
+
+  Telem(:, :) = 0.
+
+  data(:) = 0.
+  rows(:) = 0
+
+  index = 0
+  do ielem = 1, num_elems
+     xyz1 = nodes(elems(ielem, 1), :)
+     xyz2 = nodes(elems(ielem, 2), :)
+     L = sqrt(dot_product(xyz2 - xyz1, xyz2 - xyz1))
+
+     cos_xyz = (xyz2 - xyz1) / L
+     Telem(1, 1:2) = cos_xyz
+     Telem(2, 3:4) = cos_xyz
+
+     data4x4(:, :) = matmul(matmul(transpose(Telem), Kelem), Telem)
+     data4x4 = data4x4 * E / L
+
+     ind1 = 2 * (elems(ielem, 1)-1)
+     ind2 = 2 * (elems(ielem, 2)-1)
+
+     do k1 = 1, 4
+        do k2 = 1, 4
+           index = index + 1
+
+           ind_aug = 0
+           data(index) = data(index) + data4x4(k1, k2)
+           rows(index) = rows(index) + rows4x4(k1, k2) + 1
+           ind_aug = ind_aug + cols4x4(k1, k2) + 1
+
+           rows(index) = rows(index) + ones11(k1, k2) * ind1
+           ind_aug = ind_aug + ones11(k1, k2) * ind1
+
+           rows(index) = rows(index) + ones12(k1, k2) * ind1
+           ind_aug = ind_aug + ones12(k1, k2) * ind2
+
+           rows(index) = rows(index) + ones21(k1, k2) * ind2
+           ind_aug = ind_aug + ones21(k1, k2) * ind1
+
+           rows(index) = rows(index) + ones22(k1, k2) * ind2
+           ind_aug = ind_aug + ones22(k1, k2) * ind2
+
+           data(index) = data(index) * disp_aug(ind_aug)
+
+           cols(index) = ielem
+        end do
+     end do
+  end do
+
+  if (index .ne. nnz) then
+     print *, 'Error in getmtx: did not reach end of nnz vectors'
+  end if
+
+  rows(:) = rows(:) - 1
+  cols(:) = cols(:) - 1
+
+end subroutine getresder2D
+
